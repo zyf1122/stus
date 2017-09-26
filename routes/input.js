ad5666c473d06d137c4c07c1b3a967f224caa186
zengyufa <zyf1122yahu@yahoo.com>
2017-09-26 17:17:45 +0800

    yy
    yy

---
+++ routes/input.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var router = express.Router();
+
+// var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+
+// var url = 'mongodb://10.198.1.230:27017/stu';
+var db = require('../db');
+var socket = require('../socket');
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('input', { title: 'input' });
+});
+
+router.post('/', function(req, res){
+  // console.log(req.body);
+  var user = new db.user(req.body);///
+  user.save(function(err, result){
+    assert.equal(err, null);
+    // socket.io.on('mei', function(){
+    //   console.log('a');
+    // });
+    socket.io.emit('new', result);
+
+    res.redirect('/show');
+  })
+  // MongoClient.connect(url, function(err, db) {
+  //   assert.equal(null, err);
+  //   db.collection('stuinfos').insertOne(req.body, function(err, result){
+  //     assert.equal(null, err);
+  //     console.log('Connected correctly to server. ok');
+  //     db.close();
+  //   })
+  //
+  // });
+
+  // res.render('input', {})
+  // res.redirect('/show');
+})
+
+module.exports = router;
